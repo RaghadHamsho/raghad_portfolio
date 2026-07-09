ee349178e4129f8a5e332ecba6b120407b4e5da8
Raghed <raghadhamsho028@gmail.com>
2026-07-09 14:24:40 +0300

    Fix Flutter web deployment path

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -119,7 +119,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "b2f938e6cd6490fcaa1e941129f9ca78
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "114e2faef94b1cf81baf6d5300699b32",
+"flutter_bootstrap.js": "bd7bacf36d966a072b6b8acdcef5161a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
