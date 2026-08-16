05f33e0a944d6a3a31cbf105c7f784def6f9b813
Raghed <raghadhamsho028@gmail.com>
2026-08-16 14:34:01 +0300

    fixed issue

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -8,5 +8,5 @@ class AppStrings {
       'I hold a bachelor\'s degree in Technology information Engineering, specializing in Software Engineering and Information Systems.';
   static const shortdesc =
       'I have four years of experience in mobile application development, with strong proficiency in Java and object-oriented programming (OOP) concepts. My educational and professional background enables me to effectively handle the responsibilities of mobile development. I am committed to delivering optimal analytical and technical solutions, ensuring the highest standards in mobile application development';
-  static const cvUrl = 'https://drive.google.com/file/d/1PVoXX35E6DXw9rIOCpVd9i4xc2jv4Ny3/view?usp=sharing';
+  static const cvUrl = 'https://drive.google.com/file/d/17k2BL0Gkr3zwqXP1QHhrLRLR3RPFXwSi/view?usp=sharing';
 }
